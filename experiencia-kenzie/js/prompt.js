a781308f14332dab2ea1e7c70b9a5f9318c7e4e6
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-11-24 21:06:29 -0300

    chore: prompts

---
+++ experiencia-kenzie/js/prompt.js
@@ -0,0 +1,6 @@
+let name = prompt('What is your name?');
+let quest = prompt('What is your quest?');
+
+document.write('Hello, ' + name + '. ');
+document.write('Good luck on your quest ' + quest + '!');
+
