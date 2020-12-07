e01a8d1af9ec5c105ac307e06e0eead962756b0b
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-12-07 10:48:09 -0300

    update: fix at validating numbers exe

--- curso-em-video-js/exercises/exe-5/script.js
+++ curso-em-video-js/exercises/exe-5/script.js
@@ -21,17 +21,53 @@ function inList(n, l) {
 }
 
 function add() {
-    if(isNumber(num.valueAsNumber) && !inList(num.valueAsNumber), values) {
-        values.push(Number(num.valueAsNumber));
+    if(isNumber(num.value) && !inList(num.value), values) {
+        values.push(Number(num.value));
         let item = document.createElement('option');
-        item.text = `Valor ${num.valueAsNumber} adicionado.`;
+        item.text = `Valor ${num.value} adicionado.`;
         list.appendChild(item);
+        res.innerHTML = '';
     } else {
         window.alert('Valor inválido ou já adicionado na lista!');
     }
-    num.valueAsNumber = '';
+    num.value = '';
     num.focus();
 }
 
+
+function finish() {
+    if(values.length == 0) {
+        window.alert('Adicione valores antes de finalizar.');
+    } else {
+        let total = values.length;
+
+        let major = values[0];
+        let lower = values[0];
+
+        let sum = 0;
+        let media = 0;
+
+
+        for(let pos in values) {
+            sum += values[pos];
+
+            if(values[pos] > major)
+                major = values[pos];
+            if(values[pos] < lower)
+                lower = values[pos]
+        }
+
+        media = sum/total;
+        media = media.toFixed(2);
+
+        res.innerHTML = '';
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`;
+        res.innerHTML += `<p>O maior valor informado foi ${major}</p>`;
+        res.innerHTML += `<p>O menor valor informado foi ${lower}</p>`;
+        res.innerHTML += `<p>A soma de todos os valores informados é igual a ${sum}</p>`;
+        res.innerHTML += `<p>A média de todos os valores informados é igual a ${media}</p>`;
+
+    }
+}
 
 
