5d439481302423395150dbbd7bf00350388ae8cd
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-12-10 19:10:01 -0300

    chore: functions

---
+++ origamid-js-es6/exe-functions.js
@@ -0,0 +1,38 @@
+function isTruthy(data) {
+    return !!data; // !! to verify is a value is truthy or false
+}
+
+// calc of square perimeter
+
+function squarePerimeter(side) { 
+    return side * 4;
+}
+
+// full name
+
+function fullName(name, lastName) {
+    return `${name} ${lastName}`
+}
+
+// verify if the number is even 
+
+function isEven(number) {
+    if(number % 2 === 0){
+        return 'Par';
+    } else {
+        return 'Ímpar';
+    }
+}
+
+// verify data type
+
+function dataType(data) {
+    return typeof data;
+}
+
+// with addEventListener display full name when the user click on the page
+
+addEventListener('click', function() {
+    console.log('Viviane Queiroz');
+});
+
---
+++ origamid-js-es6/functions-1.js
@@ -0,0 +1,27 @@
+function imc(weight, height) { // parameters
+    imc = weight / (height ** 2);
+    return imc
+}
+
+console.log(imc(63, 1.68)); // arguments 
+
+
+// Arguments can be functions:
+
+// Usally called as "Callback" and happens -- after -- an event 
+
+
+addEventListener('click', function() {
+    console.log('Oi');
+});
+
+function showConsole() {
+    console.log('Olá')
+}
+
+addEventListener('click', showConsole); // is does not demand parenthesis because the JS understand the function has already been declared
+
+// When return is not defined, the function is going to return 'undefined', even though the code inside it's block is executed
+
+// The best practice is that a function only returns 1 data type
+
