504d1f7fb35bc30567d396697f50e171f4bd45ca
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-12-12 14:24:25 -0300

    chore: arrays and loops - part 1

---
+++ origamid-js-es6/arrays&loops.js
@@ -0,0 +1,18 @@
+var videoGames = ['Nitendo', 'PS4', 'XBoxOne'];
+
+var lastItem = videoGames.pop(); // ,pop returns AND remove the last item in the array list 
+
+videoGames.push('3DS'); // add the item at the last position of array
+
+for( var i = 0; i <= 10; i++) {
+    console.log(i);
+};
+
+var i = 0;
+
+while(i <= 100) {
+    console.log(i);
+    i++
+}
+
+
