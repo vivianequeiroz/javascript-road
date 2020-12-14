b38501a38d40b7aab9dd88fde0fbf7a6b29a13d2
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-12-14 11:53:10 -0300

    chore: dom for begginers

---
+++ origamid-js-es6/dom-iniciantes/dom.js
@@ -0,0 +1,7 @@
+const hrefPage = window.location.href;
+console.log(hrefPage);
+
+const activeElement = document.querySelector('.active');
+
+const language = window.navigator.language;
+console.log(language);
