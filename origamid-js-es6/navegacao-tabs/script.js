da83b6732d4c5de1a59f140a733e010a403441b3
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-01-04 17:28:30 -0300

    chore: tab navigation

--- origamid-js-es6/navegacao-tabs/script.js
+++ origamid-js-es6/navegacao-tabs/script.js
@@ -5,10 +5,19 @@
 const tabMenu = document.querySelectorAll('.js-tabmenu li'); 
 const tabContent = document.querySelectorAll('.js-tabcontent section'); 
 
-function activeTab(index) {
-    tabContent.forEach((section) => {
-        section.classList.remove('ativo');
+if(tabMenu.length && tabContent.length) { 
+    tabContent[0].classList.add('ativo');
+
+    function activeTab(index) {
+        tabContent.forEach((section) => {
+            section.classList.remove('ativo');
+        });
+        tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activeTab(index);
+        });
     });
 }
-
-tabContent[index].classList.add('ativo');
