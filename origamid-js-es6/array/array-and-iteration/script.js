83dd66621552bff048c45b5a5782e7392c6b28cb
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-01-16 19:24:37 -0300

    chore: filter to return only part of the array - exercise

--- origamid-js-es6/array/array-and-iteration/script.js
+++ origamid-js-es6/array/array-and-iteration/script.js
@@ -22,3 +22,12 @@ const objetosCurso = arrayCursos.map((curso) => {
 }); 
 
 console.log(objetosCurso);
+
+
+// Return a list with all the number bigger than 100
+
+const numeros = [3,656,343,2,3,5,56,7,676,85,634,523,2,99.9]; 
+
+const maiores100 = numeros.filter(n => n > 100);
+
+console.log(maiores100);
