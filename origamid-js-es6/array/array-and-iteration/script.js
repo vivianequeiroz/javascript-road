7de0774ed24f4dc7e9754405b42d5ee084c6f863
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-01-16 19:45:19 -0300

    chore: reduce to sum values in object  - exercise

--- origamid-js-es6/array/array-and-iteration/script.js
+++ origamid-js-es6/array/array-and-iteration/script.js
@@ -31,3 +31,54 @@ const numeros = [3,656,343,2,3,5,56,7,676,85,634,523,2,99.9];
 const maiores100 = numeros.filter(n => n > 100);
 
 console.log(maiores100);
+
+
+// Return the total amount of the purchases
+
+const compras = [
+    {
+        item: 'Banana',
+        preco: 'R$ 4,99'
+    },
+    {
+        item: 'Ovo',
+        preco: 'R$ 2,99'
+    },
+    {
+        item: 'Carne',
+        preco: 'R$ 25,49'
+    },
+    {
+        item: 'Refrigerante',
+        preco: 'R$ 5,35'
+    },
+    {
+        item: 'Queijo',
+        preco: 'R$ 10,60'
+    },
+]
+
+// replaces, map preco, reduce
+
+
+// const replacesCompras = compras.forEach((preco) =>{
+//     compras.replace('R$ ', '')
+// })
+
+// console.log(replaceCompras);
+
+// const comprasPreco = compras.map((compras) => {
+//     return {
+//         preco: compras.preco,
+//     }
+// });
+
+// console.log(comprasPreco);
+
+const valorTotal = compras.reduce((accumulator, item) => {
+    const precoLimpo = +item.preco.replace('R$ ', '').replace(',','.');
+    return accumulator + precoLimpo;
+}, 0);
+
+console.log(`O valor total da compra é igual a ${valorTotal}`);
+
