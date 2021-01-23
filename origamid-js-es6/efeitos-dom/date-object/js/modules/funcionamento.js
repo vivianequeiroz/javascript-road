412bee2e5331ba762dc431c3581083ac97144f43
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-01-23 19:20:06 -0300

    chore: date object

--- origamid-js-es6/efeitos-dom/date-object/js/modules/funcionamento.js
+++ origamid-js-es6/efeitos-dom/date-object/js/modules/funcionamento.js
@@ -16,3 +16,16 @@ const diasAgora = transformarDias(agora);
 const diasFuturo = transformarDias(futuro);
 
 console.log(diasFuturo - diasAgora);
+
+
+// agora.getDate(); //exactly date and time at the moment
+// agora.getDay(); //day of the week (0 - 6)
+// agora.getMonth();
+// agora.getFullYear();
+// agora.getHours();
+// agora.getMinutes();
+// agora.getTime(); // total of mile seconds since 1 jan 1970
+// agora.getUTCHours() - 3// Brasilia time
+
+// new Date() is a constructor that creates an object with 
+// values based on the intern user's computer clock 
