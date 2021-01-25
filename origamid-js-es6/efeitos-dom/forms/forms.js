e4f0efe0a9dc10dc614f95ddb5e3401befc8efd9
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-01-25 13:33:04 -0300

    chore: forms - keyup and change methods

--- origamid-js-es6/efeitos-dom/forms/forms.js
+++ origamid-js-es6/efeitos-dom/forms/forms.js
@@ -1,3 +1,19 @@
 const contato = document.querySelector('#contato');
 console.log(contato.elements.nome);
 console.log(document.forms[0]);
+
+
+// Values 
+// The property value returns the value of the element in the form
+// 'keyup' observe key per key typed by the user
+// 'change' observe the value inserted by the user when he channges the field 
+
+function handleKeyup(event) {
+    console.log(event.target.value);
+}
+
+// contato.addEventListener('keyup', handleKeyup);
+// keyup does not detect voice input data, so change can 
+// be used to overocome this problem and make the form more accessible:
+
+contato.addEventListener('change', handleKeyup);
