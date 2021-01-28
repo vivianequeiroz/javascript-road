efdcbe8dfa1d73ddb95a85edf74b6fe067fab87b
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-01-28 13:11:04 -0300

    chore: local storage to save user preferences

--- origamid-js-es6/button-creator/script.js
+++ origamid-js-es6/button-creator/script.js
@@ -40,9 +40,24 @@ function handleChange(event) {
     const value = event.target.value;
 
     handleStyle[name](value);
+    saveValues(name, value);
     showCss();
 }
 
+function saveValues(nome, value) {
+     localStorage[name] = value;
+}
+
+function setValues() {
+    const properties = Object.keys(localStorage);
+    properties.forEach(propertie => {
+        handleStyle[propertie](localStorage[propertie]);
+        controles.elements[propertie].value = localStorage[propertie];
+    });
+    showCss();
+}
+
+setValues();
 
 function showCss() {
     cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
