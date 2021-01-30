de759ed6a999b3333dabb1dc85a4553d9ab88d76
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-01-30 15:12:33 -0300

    chore: fetch - text() and methods to insert data

--- origamid-js-es6/javascript-assincrono/fetch-api/script.js
+++ origamid-js-es6/javascript-assincrono/fetch-api/script.js
@@ -1,34 +1,61 @@
-// FETCH API
+// // FETCH API
 
-// Make HTTP/HTTPS request through the method fetch(). So, the fetch method requires a local server to work properly
-// It returns a promise resolution
-// then() can be used to interact with the response, an object of type Reponse
-
-
-const doc = fetch('./doc.txt'); 
-
-console.log(doc);
-// Show a promise
-
-doc.then(resolucao => console.log(resolucao));
+// // Make HTTP/HTTPS request through the method fetch(). So, the fetch method requires a local server to work properly
+// // It returns a promise resolution
+// // then() can be used to interact with the response, an object of type Reponse
 
 
-// RESPONSE
+// const cep = fetch('https://viacep.com.br/ws/05853390/json/'); 
 
-// The object Response has a body with the content of response. It can ben transformed by 
-// using the prototype methods from the Response object. They return other promises and 
-// usually require another then() to work with the text body 
+// console.log(cep);
+// // Show a promise
 
-// doc.then(resolucao2 => {
-//     return resolucao2.text();
-// }).then((body) => {
-//     const conteudo = document.querySelector('.conteudo');
-//     conteudo.innerText = body;
-// });
+// cep.then(resolucao => console.log(resolucao));
+
+
+// // RESPONSE
+
+// // The object Response has a body with the content of response. It can ben transformed by 
+// // using the prototype methods from the Response object. They return other promises and 
+// // usually require another then() to work with the text body 
+
+// // cep.then(resolucao2 => {
+// //     return resolucao2.text();
+// // }).then((body) => {
+// //     const conteudo = document.querySelector('.conteudo');
+// //     conteudo.innerText = body;
+// // });
  
-doc
-.then( r => r.text()) // just a promise, another then() required to work with the body text
+// cep
+// .then( r => r.json()) // just a promise, another then() required to work with the body text
+// .then(body => {
+//     console.log(body.bairro);
+//     const conteudo = document.querySelector('.conteudo');
+//     conteudo.innerText = body.logradouro;
+// });
+
+
+
+// // Injection of CSS Style with text():
+
+// // const style = fetch('./style.css');
+
+// // style.then(r => r.text())
+// // .then(body => {
+// //     const conteudo = document.querySelector('.conteudo');
+// //     const css = document.createElement('style');
+// //     css.innerHTML = body;
+// //     conteudo.appendChild(css)
+// // })
+
+const sobre = fetch('./sobre.html');
+
+const div = document.createElement('div');
+
+sobre.then(r => r.text())
 .then(body => {
-    const conteudo = document.querySelector('.conteudo');
-    conteudo.innerText = body;
+    console.log(body);
+    div.innerHTML = body;
+    const titulo = div.querySelector('h1');
+    document.querySelector('h1').innerText = titulo.innerText;
 });
