1c12633a0aa90a014d5c3cca8ca7bc4394b1a43c
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-02-02 21:10:48 -0300

    chore: bitcoin value fetch

---
+++ origamid-js-es6/javascript-assincrono/fetch-api/exe-2-bitcoin-actual-value/script.js
@@ -0,0 +1,17 @@
+// Using the API https://blockchain.info/ticker 
+// return in DOM the purchase value of bitcoin in reais
+// Update this value each 30seconds
+
+
+const btcDisplay = document.querySelector('.btc');
+
+
+function fetchBtc() {
+    fetch('https://blockchain.info/ticker')
+    .then(response => response.json())
+    .then(btcJson => {
+        btcDisplay.innerText = 'R$ ' + btcJson.BRL.buy;
+    });
+}
+
+fetchBtc();
