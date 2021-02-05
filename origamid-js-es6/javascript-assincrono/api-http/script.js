e29df281142edf10515a330bc1d05959ebf69746
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-02-05 16:45:13 -0300

    chore: http and headers

--- origamid-js-es6/javascript-assincrono/api-http/script.js
+++ origamid-js-es6/javascript-assincrono/api-http/script.js
@@ -5,3 +5,40 @@
 // Interface - commands to interact with the application
 
 
+// HTTP
+
+// Hypertext Transfer Protocol
+// Request made through an URL
+// The pattern is GET method
+// 
+
+
+
+
+
+
+
+
+
+
+
+fetch('https://pokeapi.co/api/v2/pokemon/')
+.then(response => response.json(){
+    .then(pokemon => {
+        console.log(pokemon);
+    })
+})
+
+
+const url = 'htpps://jsonplaceholder.typicode.com/posts';
+const options = {
+    method: 'POST',
+    body: '{"title": "JavaScript"}',
+    headers: {
+        "Content-Type": "application/json; charset-utf-8"
+    }
+}
+
+fetch(url, options)
+.then(response => response.json())
+.then(json => console.log(json));
