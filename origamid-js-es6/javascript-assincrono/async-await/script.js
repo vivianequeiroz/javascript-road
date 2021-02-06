8d280962871ebcefcf4b883e9f55292c827af9df
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-02-06 15:57:27 -0300

    chore: async await

---
+++ origamid-js-es6/javascript-assincrono/async-await/script.js
@@ -0,0 +1,37 @@
+// async await
+
+// async => assyncronous parts that must need resolved before continue with the function 
+// await => indicates the promise we must wait for being resolved (ES8 feature)
+
+// Comparing with .then, async await has a clearer syntax 
+
+// WITH .THEN
+// function iniciarFetch() {
+//      fetch('./dados.json')
+//      .then(dadosResponse => dadosResponse.json())
+//      .then(dadosJSON => {
+//       document.body.innerText = dadosJSON.titulo;
+//})
+//}
+// iniciarFetch();
+
+
+// WITH ASYNC AWAIT
+// async function iniciarAsync() {
+//     const dadosResponse = await fetch('./dados.json);
+//     const dadosJSON = await dadosResponse.json();
+//     document.body.innerText = dadosJSON.titulo;
+// }
+// iniciarAsync();
+
+
+
+async function puxarDados() {
+    const dadosObjetoResponse = await fetch('./dados.json');
+    console.log(dadosObjetoResponse);
+    const dadosJSON = await dadosObjetoResponse.json();
+    console.log(dadosJSON);
+    documeent.body.innerText = dadosJSON.titulo;
+}
+
+puxarDados();
