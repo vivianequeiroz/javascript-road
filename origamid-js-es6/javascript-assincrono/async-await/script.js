09e04e27084fdd44a6c9422d82e78fbe9e11f56d
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-02-06 15:58:11 -0300

    chore: async await vs .then

--- origamid-js-es6/javascript-assincrono/async-await/script.js
+++ origamid-js-es6/javascript-assincrono/async-await/script.js
@@ -3,9 +3,19 @@
 // async => assyncronous parts that must need resolved before continue with the function 
 // await => indicates the promise we must wait for being resolved (ES8 feature)
 
+async function puxarDados() {
+    const dadosObjetoResponse = await fetch('./dados.json');
+    console.log(dadosObjetoResponse);
+    const dadosJSON = await dadosObjetoResponse.json();
+    console.log(dadosJSON);
+    documeent.body.innerText = dadosJSON.titulo;
+}
+
+puxarDados();
+
 // Comparing with .then, async await has a clearer syntax 
 
-// WITH .THEN
+// .THEN
 // function iniciarFetch() {
 //      fetch('./dados.json')
 //      .then(dadosResponse => dadosResponse.json())
@@ -16,22 +26,10 @@
 // iniciarFetch();
 
 
-// WITH ASYNC AWAIT
+// ASYNC AWAIT
 // async function iniciarAsync() {
 //     const dadosResponse = await fetch('./dados.json);
 //     const dadosJSON = await dadosResponse.json();
 //     document.body.innerText = dadosJSON.titulo;
 // }
 // iniciarAsync();
-
-
-
-async function puxarDados() {
-    const dadosObjetoResponse = await fetch('./dados.json');
-    console.log(dadosObjetoResponse);
-    const dadosJSON = await dadosObjetoResponse.json();
-    console.log(dadosJSON);
-    documeent.body.innerText = dadosJSON.titulo;
-}
-
-puxarDados();
