8acb3599a2e6f75391a2176f1da5e1aa79b6e793
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-02-07 08:09:51 -0300

    fix: prevent that the animals count does not occur

--- origamid-js-es6/javascript-assincrono/animais-fetch/js/modules/fetchanimais.js
+++ origamid-js-es6/javascript-assincrono/animais-fetch/js/modules/fetchanimais.js
@@ -1,25 +1,27 @@
-export default function initFetchAnimais() {
+import initAnimaNumeros from './anima-numeros.js';
 
+export default function initFetchAnimais() { 
+    async function fetchAnimais(url) {
+        const animaisResponse = await fetch(url);
+        const animaisJSON = await animaisResponse.json();
+        const numerosGrid = document.querySelector('.numeros-grid');
+    
+    
+        animaisJSON.forEach(animal => {
+           const divAnimal = createAnimal(animal);
+           numerosGrid.appendChild(divAnimal);
+        });
+    
+        initAnimaNumeros(); // to prevent that it loads first than the fetch
+    }
+    
+    function createAnimal(animal) {
+        const div = document.createElement('div');
+        div.classList.add('numero-animal');
+        div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;  
+    
+        return div;
+    }
+    
+    fetchAnimais('./animaisapi.json');
 }
-
-async function fetchAnimais(url) {
-    const animaisResponse = await fetch(url);
-    const animaisJSON = await animaisResponse.json();
-    const numerosGrid = document.querySelector('.numeros-grid');
-
-
-    animaisJSON.forEach(animal => {
-       const divAnimal = createAnimal(animal);
-       numerosGrid.appendChild(divAnimal);
-    });
-}
-
-function createAnimal(animal) {
-    const div = document.createElement('div');
-    div.classList.add('numero-animal');
-    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;  
-
-    return div;
-}
-
-fetchAnimais('./animaisapi.json');
--- origamid-js-es6/javascript-assincrono/animais-fetch/js/script.js
+++ origamid-js-es6/javascript-assincrono/animais-fetch/js/script.js
@@ -6,7 +6,6 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetchanimais.js';
 
@@ -19,6 +18,5 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
 initFetchAnimais();
