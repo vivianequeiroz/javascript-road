f87c232b113ec59c30f03bf8bed71bb6b7338637
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-02-13 18:13:13 -0300

    chore: constructor, this and static

--- origamid-js-es6/classes/script.js
+++ origamid-js-es6/classes/script.js
@@ -15,18 +15,24 @@
 
 
 // Class: 
+// the method constructor(args) {} receive all the properties the obj will have
+// this. makes a reference to the obj created with new, and allow us to access the class's methods 
 
 class Button {
     constructor(text, background) {
     this.text = text;
     this.background = background;
     }
-
     element() {
     const buttonElement = document.createElement('button');
     buttonElement.innerText = this.text;
     buttonElement.style.background = this.background;
     return buttonElement;
+    }
+    appendTo(target) {
+        const targetElement = document.querySelector(target);
+        targetElement.appendChild(this.element());
+        return targetElement;
     }
 }
 
@@ -34,3 +40,36 @@ const blueButton = new Button('Compras', 'blue');
 
 
 console.log(blueButton.element());
+console.log(blueButton.appendTo('body'));
+
+
+
+
+// Static - allow us to create method directly in the class since that the methods created always
+// are connected to the class prototype. 
+// It is similar to the Array.from that is a method from the Array constructor, instead of [].map, that 
+// is a method from an Array
+
+class Button {
+    constructor(options) {
+        this.options = options;
+    }
+
+    static createButton(text, background) {
+        const buttonElement = document.createElement('button');
+        buttonElement.innerText = this.text;
+        buttonElement.style.background = this.background;
+        return buttonElement;
+    }
+}
+
+const optionBlue = {
+     backgroundColor: 'blue',
+     text: 'Comprar',
+     color: 'white',
+}
+
+const blueButton = new Button(optionBlue);
+
+
+const blueButtonStatic = Button.createButton('Clique', 'blue');
