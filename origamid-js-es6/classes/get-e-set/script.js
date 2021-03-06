199ac4309413955e7c39957613c75d4f1b1f49df
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-03-06 19:18:12 -0300

    chore: get and set with classes

--- origamid-js-es6/classes/get-e-set/script.js
+++ origamid-js-es6/classes/get-e-set/script.js
@@ -16,3 +16,23 @@ const frutas = {
 
 frutas.nova = 'Banana';
 // accessed as a property
+
+class Button {
+    constructor(text,color) {
+        this.text = text;
+        this.color = color;
+    }
+    get element() {
+        const type = this._elementType || 'button'
+        const buttonElement = document.createElement('button');
+        buttonElement.innerText = this.text;
+        buttonElement.style.color = this.color;
+        return buttonElement;
+    }
+    set element(type) {
+        this._elementTupe = type;
+    }
+}
+
+
+const blueButton = new Button('Comprar', 'blue')
