3487ba312f19d9dba3f0b3de5de4d01a5e68eaf1
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-03-06 19:43:21 -0300

    chore: super in classes

--- origamid-js-es6/classes/extends/script.js
+++ origamid-js-es6/classes/extends/script.js
@@ -2,6 +2,10 @@
 // It has the acess to the main class methods through it's prototype
 
 
+// Super - to acess the main class and it's methods and properties
+// It allows the manipulation of the main method or property
+
+
 class Veiculo {
     constructor(rodas) {
         this.rodas = rodas;
@@ -12,12 +16,20 @@ class Veiculo {
 }
 
 class Moto extends Veiculo {
+    constructor(rodas, capacete) {
+        super(rodas, capacete); // super(all the parameters of the original constructor + the new that will be created)
+        this.capacete = capacete;
+    }
+    acelerar() {
+        super.acelerar();
+        console.log('Acelerou rápido.');
+    }
     empinar() {
         console.log('Empinou com ' + this.rodas + ' rodas.');
     }
 }
 
 
-const honda = new Moto(2);
-honda.empinar();
+const honda = new Moto(2, true);
+
 
