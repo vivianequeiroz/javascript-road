1480aafb6fa78c284b4f79533faa01874b9ae51b
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-13 12:31:54 -0300

    chore: cleaning and formatting CPF

--- origamid-js-es6/nodelist-para-array/script.js
+++ origamid-js-es6/nodelist-para-array/script.js
@@ -1,7 +1,42 @@
-const cpfs = document.querySelectorAll('.cpf li');
+// simple function to transform node list into array:
 
-function elementsInnerText([...elements]) {
-    return elements.map(element => element.innerText);
+// const cpfs = document.querySelectorAll('.cpf li');
+
+// function elementsInnerText([...elements]) {
+//     return elements.map(element => element.innerText);
+// }
+
+// console.log(elementsInnerText(cpfs));
+
+
+
+// cleaning cpf
+
+const cpfsList = document.querySelectorAll('.cpf li');
+
+const elementsInnerText = ([...elements]) => {
+  return elements.map(element => element.innerText);
 }
 
-console.log(elementsInnerText(cpfs));
+const limparCPF = (cpf) => {
+  return cpf.replace(/\D/g, '');
+}
+
+const construirCPF = (cpf) => {
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
+}
+
+const formatarCPFS = (cpfs) => {
+  return cpfs.map(limparCPF).map(construirCPF);
+}
+
+const substituiCPFS = (cpfsElements) => {
+  const cpfs = elementsInnerText(cpfsElements);
+  const cpfsFormatados = formatarCPFS(cpfs);
+
+  cpfsElements.forEach((element, index) => {
+    element.innerText = cpfsFormatados[index];
+  })
+}
+
+substituiCPFS(cpfsList);
