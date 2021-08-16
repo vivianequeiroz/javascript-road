6cefa0fa0fdb27f589deb26caddfe2d678db54a1
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-08-16 15:55:18 -0300

    chore: arrow functions

---
+++ functional-js/function-anatomy/arrow-function.js
@@ -0,0 +1,13 @@
+//Function expression
+const incrementalFunc = function (value) {
+  return value + 1;
+};
+
+//Arrow functions are always anonymous Functions
+const incrementalArrow = (value) => {
+  return value + 1;
+};
+                                            //implicit return withou brackets
+const incrementalArrowSimplified = (value) => value + 1;
+
+
