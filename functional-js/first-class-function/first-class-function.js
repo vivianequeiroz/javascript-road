1a2178c05a338b05bbb4c497ed70352bcb6b9afc
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-08-16 16:56:36 -0300

    chore: first class function

---
+++ functional-js/first-class-function/first-class-function.js
@@ -0,0 +1,14 @@
+// Functions are treated as any other variable
+
+const add = (a, b) => a + b; 
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+console.log(add(1, 2)); 
+console.log(subtract(1, 2)); 
+console.log(multiply(1, 2)); 
+console.log(divide(1, 2)); 
