902a8244bbad08a146dc4d111dce2d4c0e7e08f6
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-08-18 20:58:29 -0300

    chore: higher order functions - curring

--- functional-js/higher-order-functions/higher-order-function.js
+++ functional-js/higher-order-functions/higher-order-function.js
@@ -1,3 +1,5 @@
+//Functions as a parameter
+
 //Functions that are passed as a parameter to another function or are returned
 // by it
 
@@ -21,3 +23,22 @@ run(function () {
 const result = run(Math.random); //without the return value at run() it returns
                                 // undefined
 console.log(result);
+
+
+
+
+// Function as a return
+
+// function finalPrice(tax, price) {
+//   return price * (1 + tax);
+// }
+
+
+function finalPrice(tax) {
+  return function(price) {
+    return price * (1 + tax);
+  }
+}
+const brFinalPrice = finalPrice(0.0875);
+
+console.log(brFinalPrice(25.1));
